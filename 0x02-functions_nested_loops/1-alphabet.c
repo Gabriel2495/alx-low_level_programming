74272e74fe14d002b51f0a3dfa990ae9ee9fdfa7
Gabriel Sitezeu <gabrielsitezeutowen@gmail.com>
2022-09-13 18:40:24 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints alphabets 'a - z'
+*/
+void main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
